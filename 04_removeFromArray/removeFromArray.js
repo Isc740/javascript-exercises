ef04b214ea3e7bc3ad96bf183e6e5013eca1f5ca
Isc740 <isaazcantillo@gmail.com>
2024-11-09 20:15:10 -0500

    complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-};
+const removeFromArray = (arr, ...args) =>
+  arr.filter((val) => !args.includes(val));
 
 // Do not edit below this line
 module.exports = removeFromArray;
