8b4f9dc8d63c0911f4a7978ad7d7a03cd7b32cad
Isc740 <isaazcantillo@gmail.com>
2024-11-10 09:32:10 -0500

    complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    typeof num1 !== "number" || typeof num2 !== "number" || num1 % 1 !== 0 ||
+    num2 % 1 !== 0 || !num1 || !num2 ||
+    num1 < 0 || num2 < 0
+  ) return "ERROR";
+  biggerNumber = num1 > num2 ? num1 : num2;
+  smallerNumber = num1 > num2 ? num2 : num1;
 
+  let result = 0;
+  for (let i = smallerNumber; i <= biggerNumber; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
