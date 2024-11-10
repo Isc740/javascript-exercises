f86b73ba026c5075b1161f8146378636ede12461
Isc740 <isaazcantillo@gmail.com>
2024-11-10 18:49:26 -0500

    complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (n1, n2) {
+  return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function (n1, n2) {
+  return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+  for (const item of arr) {
+    sum += item;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let sum = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    sum *= arr[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function (n1, n2) {
+  return Math.pow(n1, n2);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) return 1;
+  result = 1;
+  for (let i = n; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
