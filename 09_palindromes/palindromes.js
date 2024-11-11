96995ac5332846828d8c2667d93393843fe6d057
Isc740 <isaazcantillo@gmail.com>
2024-11-10 21:08:42 -0500

    complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const removeNonAlphanumeric = (string) => string.replace(/\W/g, '');
 
+const palindromes = function (word) {
+  filteredWord = removeNonAlphanumeric(word).toLowerCase();
+  let left = filteredWord.length - 1;
+  for (let i = 0; i < filteredWord.length / 2; i++) {
+    if (filteredWord[left] !== filteredWord[i]) {
+      return false;
+    }
+    left--;
+  }
+  return true;
 };
 
 // Do not edit below this line
