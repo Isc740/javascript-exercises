75c3e8f5a3f4001aa416bd0c61121736ed4498d9
Isc740 <isaazcantillo@gmail.com>
2024-11-19 14:32:49 -0500

    complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (position) {
+  position = parseInt(position);
 
+  if (position === 0) return 0;
+  if (position < 1) return "OOPS";
+
+  let prevNum = 1;
+  let temp = 1;
+  let currentNum = 1;
+  for (let i = 2; i <= position; i++) {
+    if (i == position) {
+      return currentNum;
+    }
+
+    temp = currentNum;
+    currentNum = prevNum + currentNum;
+    prevNum = temp;
+  }
+  return currentNum;
 };
+
+fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
